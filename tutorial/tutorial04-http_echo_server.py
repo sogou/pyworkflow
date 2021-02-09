f8524464c062bf2f33290a98a2538e7f97a036e5
Ozzy <cfhamlet@gmail.com>
2021-02-09 14:09:48 +0800

    add: more pythonic

--- tutorial/tutorial04-http_echo_server.py
+++ tutorial/tutorial04-http_echo_server.py
@@ -4,59 +4,53 @@ import threading
 
 import pywf as wf
 
-cv = threading.Condition()
 
+def handle(task):
+    request = task.get_req()
+    response = task.get_resp()
 
-def Stop(signum, frame):
-    print("Stop server:", signum)
-    cv.acquire()
-    cv.notify()
-    cv.release()
-
-
-def process(t):
-    req = t.get_req()
-    resp = t.get_resp()
-    seq = t.get_task_seq()
-    resp.append_body(b"<html>\n")  # as bytes
-    resp.append_body(
-        "<p>{} {} {}</p>\n".format(
-            req.get_method(), req.get_request_uri(), req.get_http_version()
-        )
+    response.append_body(b"<html>\n")  # as bytes
+    response.append_body(
+        f"<p>{request.get_method()} "
+        f"{request.get_request_uri()} "
+        f"{request.get_http_version()}</p>\n"
     )
-    headers = req.get_headers()
-    for h in headers:
-        resp.append_body("<p>{}: {}</p>\n".format(h[0], h[1]))
-    resp.append_body("</html>\n")  # as str
-    resp.set_http_version("HTTP/1.1")
-    resp.set_status_code("200")
-    resp.set_reason_phrase("OK")
-    resp.add_header_pair("Content-Type", "text/html")
-    resp.add_header_pair("Server", "Sogou Python3 WFHttpServer")
-    if seq == 9:
+    headers = request.get_headers()
+    for header in headers:
+        response.append_body(f"<p>{header[0]}: {header[1]}</p>\n")
+    response.append_body("</html>\n")  # as str
+
+    response.set_http_version("HTTP/1.1")
+    response.set_status_code("200")
+    response.set_reason_phrase("OK")
+    response.add_header_pair("Content-Type", "text/html")
+    response.add_header_pair("Server", "Sogou Python3 WFHttpServer")
+    seq = task.get_task_seq()
+    if seq == 9:  # close after 9 requests
         resp.add_header_pair("Connection", "close")
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage {} <port>".format(sys.argv[0]))
-        sys.exit(1)
+
     port = int(sys.argv[1])
-    signal.signal(signal.SIGINT, Stop)
-    server = wf.HttpServer(process)
-    # You can use server.start(socket.AddressFamily.AF_INET, "localhost", port) too
-    if server.start(port) == 0:
-        cv.acquire()
-        cv.wait()
-        cv.release()
-        server.stop()
-        """ server.stop() equal to:
-        server.shutdown()
-        server.wait_finish()
-        """
-    else:
-        print("Cannot start server")
-        sys.exit(1)
+
+    server = wf.HttpServer(handle)
+    stop_event = threading.Event()
+
+    def stop(*args):
+        if not stop_event.is_set():
+            stop_event.set()
+
+    for sig in (signal.SIGTERM, signal.SIGINT):
+        signal.signal(sig, stop)
+
+    code = server.start(port)
+    if code != 0:
+        print(f"Start server fail, code={code}")
+        sys.exit(code)
+
+    stop_event.wait()
+    server.stop()
 
 
 if __name__ == "__main__":
